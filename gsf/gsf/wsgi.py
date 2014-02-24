2cca49fe72f3072e7f7760f35eee6c77a7b4d2d9
Bardia Keyoumarsi <bkeyouma@ucsc.edu>
2014-02-24 23:58:45 +0000

    changed wsgi config

--- gsf/gsf/wsgi.py
+++ gsf/gsf/wsgi.py
@@ -7,7 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+sys.path.append('/web/open-fusion-webapp/gsf')
+sys.path.append('/web/open-fusion-webapp/gsf/gsf/')
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gsf.settings")
 
 from django.core.wsgi import get_wsgi_application
