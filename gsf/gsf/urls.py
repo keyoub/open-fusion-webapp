46d74431d7c53377086697a2adb45413056a5a22
Bardia Keyoumarsi <bkeyouma@ucsc.edu>
2014-02-26 20:31:40 -0800

    took out dead code

--- gsf/gsf/urls.py
+++ gsf/gsf/urls.py
@@ -8,7 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'gsf.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^oauth2/', include('provider.oauth2.urls', namespace = 'oauth2')),
     url(r'^show/', include('show.urls')),
     url(r'^api/', include('api.urls')),
     #url(r'^admin/', include(admin.site.urls)),
