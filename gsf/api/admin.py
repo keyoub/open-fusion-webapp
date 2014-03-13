bbd0ed208b0b21186710cd353d86da2f25be0eb7
Bardia Keyoumarsi <bkeyouma@ucsc.edu>
2014-03-12 21:28:00 -0700

    Finished admin section and iOS key generation

--- gsf/api/admin.py
+++ gsf/api/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
 from api.models import Features, APIKey
+from random import randint
 
 class APIKeyAdmin(admin.ModelAdmin):
-   fields = ['application', 'organization',
-             'dev_name', 'email', 'upload', 'download']
+   fieldsets = (
+      ('Generate iOS API Key', {
+         'fields': ('application', 'organization',
+                    'dev_name', 'email','key')
+      }),
+   )
+
+   readonly_fields = ('key',)
+   
+   list_display = ('dev_name', 'email', 'key')
+
+   def save_model(self, request, obj, form, change):
+      obj.key = randint(10000000, 99999999)
+      obj.upload = True
+      obj.save() 
 
 admin.site.register(APIKey, APIKeyAdmin)
