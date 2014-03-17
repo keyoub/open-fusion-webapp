5f3488899613f66584e87c1dc770e19136e3db92
Bardia Keyoumarsi <bkeyouma@ucsc.edu>
2014-03-16 23:20:16 -0700

    Changed the query UI to accept addresses

--- gsf/home/views.py
+++ gsf/home/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
+from django.core.exceptions import ValidationError
 from django import forms
-from ogre import OGRe
 from gsf.settings import BASE_DIR
+from pygeocoder import Geocoder
+from ogre import OGRe
 import os, io, json, time
+
+"""def validate_addr(value):
+
+   # Check if the address is valid
+   if not Geocoder.geocode(value).valid_address:
+      raise forms.ValidationError("Please enter a valid address")
+"""
+   
+      
 
 class TwitterForm(forms.Form):
    keywords = forms.CharField(required=False, help_text="Space separated keywords")
-   lat      = forms.FloatField(label='Latitude', required=True )
-   lon      = forms.FloatField(label='Longitude', required=True )
+   #lat      = forms.FloatField(label='Latitude', required=True )
+   #lon      = forms.FloatField(label='Longitude', required=True )
+   addr     = forms.CharField(required=True, max_length=500, label='Address',
+                help_text='eg. Santa Cruz, CA or Mission st, San Francisco')
    radius   = forms.FloatField(required=True)
    t_from   = forms.DateTimeField(label='From', required=False,
                                   input_formats=['%Y-%m-%d %H:%M:%S'])
@@ -16,20 +29,26 @@ class TwitterForm(forms.Form):
                                   input_formats=['%Y-%m-%d %H:%M:%S'])
    #text     = forms.BooleanField()
    #images   = forms.BooleanField()
-   
-
 
 def index(request):
    if request.method == 'POST':
       form = TwitterForm(request.POST)
       
       if form.is_valid():
+         lat, lon = 0, 0
          keywords = form.cleaned_data['keywords']
-         lat = form.cleaned_data['lat']
-         lon = form.cleaned_data['lon']
+         #lat = form.cleaned_data['lat']
+         #lon = form.cleaned_data['lon']
+         addr = form.cleaned_data['addr']
          radius = form.cleaned_data['radius']
          t_from = form.cleaned_data['t_from']
          t_to = form.cleaned_data['t_to']
+
+         results = Geocoder.geocode(addr)
+
+         lat = float(results[0].coordinates[0])
+         lon = float(results[0].coordinates[1])
+
          params = {
                      "where": (lat, lon, radius, "km"),
                      "what" : ("text",)
@@ -38,9 +57,9 @@ def index(request):
          # Get time span and convert to epoch time
          if t_from and t_to:
             t_from = int(time.mktime(
-                           time.strptime(str(t_from)[:19], '%Y-%m-%d %H:%M:%S'))) - time.timezone
+               time.strptime(str(t_from)[:19], '%Y-%m-%d %H:%M:%S'))) - time.timezone
             t_to   = int(time.mktime(
-                           time.strptime(str(t_to)[:19], '%Y-%m-%d %H:%M:%S'))) - time.timezone
+               time.strptime(str(t_to)[:19], '%Y-%m-%d %H:%M:%S'))) - time.timezone
             params['when'] = (t_from, t_to)
          
          # Get twitter data
