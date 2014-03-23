68f3a5e725922488572ca977fb7afb4437a561b0
Bardia Keyoumarsi <bkeyouma@ucsc.edu>
2014-03-23 14:40:48 -0700

    Added error detection on home page UI and Datetime picker for the form

--- gsf/gsf/settings.py
+++ gsf/gsf/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap3',
+    'bootstrap3_datetime',
     'ogre',
     'home',
     'show',
--- gsf/home/views.py
+++ gsf/home/views.py
@@ -1,34 +1,49 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.exceptions import ValidationError
+from bootstrap3_datetime.widgets import DateTimePicker
 from django import forms
 from gsf.settings import BASE_DIR
 from pygeocoder import Geocoder
 from ogre import OGRe
 import os, io, json, time, hashlib, datetime
 
+
 """
    The UI input form for the twitter retriever
 """
 class TwitterForm(forms.Form):
    keywords = forms.CharField(required=False, help_text="Space separated keywords")
-   addr     = forms.CharField(required=True, max_length=500, label='Address',
+   addr     = forms.CharField(required=True, max_length=500, label='*Address',
                 help_text='eg. Santa Cruz, CA or Mission st, San Francisco')
-   radius   = forms.FloatField(required=True, 
+   radius   = forms.FloatField(required=True, label='*Radius',
                 help_text='in Kilometers')
-   t_from   = forms.DateTimeField(label='From', required=False,
-                                  input_formats=['%Y-%m-%d %H:%M:%S'],
-                                  widget=forms.TextInput(attrs={"data-format":"yyyy/MM/dd hh:mm:ss"}))
-   t_to     = forms.DateTimeField(label='To', required=False,
-                                  input_formats=['%Y-%m-%d %H:%M:%S'],
-                                  widget=forms.TextInput(attrs={"data-format":"yyyy/MM/dd hh:mm:ss"}))
+   t_from   = forms.DateTimeField(required=False, label='From',
+               help_text='Enter starting date and time',
+               widget=DateTimePicker(options={"format": "YYYY-MM-DD HH:mm:ss"}))
+   t_to     = forms.DateTimeField(required=False, label='To',
+               help_text='Enter ending date and time',
+               widget=DateTimePicker(options={"format": "YYYY-MM-DD HH:mm:ss"}))
    text     = forms.BooleanField(required=False)
    images   = forms.BooleanField(required=False)
 
 """
+   Helper function that builds the context for index.html 
+"""
+def form_errors(address_flag, no_result_flag, time_flag):
+   form = TwitterForm()
+   context = {
+                'form': form,
+                'address_flag': address_flag,
+                'no_result_flag': no_result_flag,
+                'time_flag': time_flag,
+             }
+   return context
+
+"""
    The home page query UI controller.
       - Handles queries on local db
-      - Handle queries for the retriever
+      - Handles queries for the retriever
 """
 def index(request):
    if request.method == 'POST':
@@ -36,7 +51,11 @@ def index(request):
       
       # Get query parameters
       if form.is_valid():
+         # Initialize variables and flags
+         address_flag, no_result_flag, time_flag = False, False, False
          lat, lon = 0, 0
+         
+         # Get data from the from
          keywords = form.cleaned_data['keywords']
          addr = form.cleaned_data['addr']
          radius = form.cleaned_data['radius']
@@ -51,7 +70,10 @@ def index(request):
             lat = float(results[0].coordinates[0])
             lon = float(results[0].coordinates[1])
          except:
-            pass
+            # If the geocoder API doesn't return with results
+            # return the user to home page with the address error flag
+            return render(request, 'home/index.html', 
+                     form_errors(True, no_result_flag, time_flag))
 
          # Start building the query for the retriever
          params = {
@@ -72,10 +94,19 @@ def index(request):
             t_to   = int(time.mktime(
                time.strptime(str(t_to)[:19], '%Y-%m-%d %H:%M:%S'))) - time.timezone
             params['when'] = (t_from, t_to)
-         
+         # Return to home page with error if only one field id provided
+         elif t_from or t_to:
+            return render(request, 'home/index.html', 
+                     form_errors(address_flag, no_result_flag, True))
+
          # Get twitter data
          data = OGRe.get(("twitter",), keywords, params)
-         
+
+         # Return to home page if no tweets were found
+         if not data['features']:
+            return render(request, 'home/index.html', 
+                     form_errors(address_flag, True, time_flag))
+
          # The center pin for the visualizer
          epicenter = {
                         "type": "FeatureCollection",
