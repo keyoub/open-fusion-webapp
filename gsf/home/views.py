c658a80fd8e883caaee06c611090208d8bc3c781
Bardia Keyoumarsi <bkeyouma@ucsc.edu>
2014-05-05 14:38:11 -0700

    Cleaning up the large home/views.py file into modules for easier coding structure.

--- gsf/home/views.py
+++ gsf/home/views.py
@@ -1,19 +1,15 @@
+import os, io, json, time, hashlib, datetime, logging, random
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpResponseRedirect
-from django.core.exceptions import ValidationError
-from bootstrap3_datetime.widgets import DateTimePicker
-from django import forms
-from django.forms.formsets import formset_factory
+from django.http import HttpResponseRedirect
 from django.core.mail import send_mail
 from django.core.exceptions import PermissionDenied
-from mongoengine.queryset import Q
 from gsf.settings import BASE_DIR, TWITTER_CONSUMER_KEY, \
                          TWITTER_ACCESS_TOKEN
+from twython import TwythonRateLimitError, TwythonError
 from api.models import Features, APIKey, Coordinates
+from queryforms import *
 from pygeocoder import Geocoder
 from ogre import OGRe
-from twython import TwythonRateLimitError, TwythonError
-import os, io, json, time, hashlib, datetime, logging, random
 
 logger = logging.getLogger(__name__)
 
@@ -23,70 +19,6 @@ retriever = OGRe ({
       "access_token": TWITTER_ACCESS_TOKEN,
    }
 })
-
-# Choices variables for the forms select fields
-TWITTER_CHOICES = (
-   ("image", "Images"),
-   ("text", "Text"),
-)
-
-GSF_IMAGE_CHOICES = (
-   ("imf", "With faces detected"),
-   ("imb", "With bodies detected"),
-)
-
-OPERATORS = (
-   ("", "="),
-   ("__gt", ">"),
-   ("__lt", "<"),
-   ("__gte", ">="),
-   ("__lte", "<="),
-)
-
-LOGICALS = (
-   (0, "OR"),
-   (1, "AND"), 
-)
-
-"""
-   The Epicenters form constructor for GSF data querying
-"""
-class GSFFusionForm(forms.Form):
-   images   = forms.MultipleChoiceField(required=False, choices=GSF_IMAGE_CHOICES,
-             widget=forms.CheckboxSelectMultiple())
-   temperature_logic  = forms.ChoiceField(label="Temperature",
-                     required=False, choices=OPERATORS)
-   temperature = forms.DecimalField(label="",required=False,
-                     help_text="eg. Temperature >= 60 &deg;F",
-                     min_value=1, max_value=150)
-   humidity_logic  = forms.ChoiceField(label="Humidity",
-                     required=False, choices=OPERATORS)
-   humidity = forms.DecimalField(label="",required=False,
-                  help_text="eg. humidity <= 60 %",
-                  min_value=0, max_value=100)
-   noise_level_logic  = forms.ChoiceField(label="Noise Level",
-                     required=False, choices=OPERATORS)
-   noise_level = forms.DecimalField(label="",required=False,
-                     help_text="eg. Noise level < 80 dB",
-                     min_value=-120, max_value=100)
-   
-"""
-   The Aftershocks form constructor for GSF data querying
-"""
-class MiscForm(forms.Form):
-   radius = forms.FloatField(required=False, label="Aftershock Radius",
-                help_text="in Kilometers", min_value = 0.1, max_value=5)
-
-"""
-   The Epicenters form constructor for Twitter querying
-"""
-class TwitterFusionForm(forms.Form):
-   options = forms.MultipleChoiceField(required=False, choices=TWITTER_CHOICES,
-                widget=forms.CheckboxSelectMultiple())
-   keywords = forms.CharField(required=False, help_text="eg. Wild OR Stallions")
-   number = forms.DecimalField(required=False, label="Number of Tweets",
-            min_value = 1, max_value = 15, help_text="""Default 1, Max 15. 
-            Beware that large requests take a long time to get back from twitter""")
 
 """
    Query the local db for cached third party data before
@@ -497,25 +429,6 @@ def send_coordinates(request):
                  })
    else:
       raise PermissionDenied
-      
-"""
-   The UI input form for the twitter retriever
-"""
-class TwitterForm(forms.Form):
-   keywords = forms.CharField(required=False, help_text="Space separated keywords")
-   addr     = forms.CharField(required=True, max_length=500, label="*Address",
-                help_text="eg. Santa Cruz, CA or Mission st, San Francisco")
-   radius   = forms.FloatField(required=True, label="*Radius",
-                help_text="in Kilometers")
-   t_from   = forms.DateTimeField(required=False, label="From",
-               help_text="Enter starting date and time",
-               widget=DateTimePicker(options={"format": "YYYY-MM-DD HH:mm:ss"}))
-   t_to     = forms.DateTimeField(required=False, label="To",
-               help_text="Enter ending date and time",
-               widget=DateTimePicker(options={"format": "YYYY-MM-DD HH:mm:ss"}))
-   text     = forms.BooleanField(required=False)
-   images   = forms.BooleanField(required=False)
-
 
 """
    The home page query UI controller.
