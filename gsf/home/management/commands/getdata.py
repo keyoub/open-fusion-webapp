e569d860eb2d12ea7e5b1baec0986fec379a19e7
Bardia Keyoumarsi <bkeyouma@ucsc.edu>
2014-05-14 22:35:45 +0000

    Minor changes to the getdata script. (v3.5.3)

--- gsf/home/management/commands/getdata.py
+++ gsf/home/management/commands/getdata.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand, CommandError
 from gsf.settings import TWITTER_CONSUMER_KEY, TWITTER_ACCESS_TOKEN
 from api.models import OgreQueries, Features
 from ogre import OGRe
-import logging, json
+import logging, json, datetime
 
 logger = logging.getLogger(__name__)
 
@@ -11,6 +11,7 @@ class Command(BaseCommand):
    help = "Gets data using the retriever and saved user queries"
    
    def handle(self, *args, **options):
+      logger.info("getdata was successfully invoked at %s" % datetime.datetime.now())
       query_limit = 1
       for limit in args:
          query_limit = limit
@@ -24,7 +25,6 @@ class Command(BaseCommand):
       
       queries = OgreQueries.objects.all().as_pymongo()[:350]
       for query in queries:
-         # TODO: add limit to number of queries to runl
          query.pop("_id", None)
          query.pop("date_added", None)
          if query.get("location", None):
